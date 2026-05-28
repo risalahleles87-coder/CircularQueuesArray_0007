b3500142b0e7b6133e0df36793c6f39b0da2dce1
risalahlele87-coder <risalahleles87@gmail.com>
2026-05-28 15:06:37 +0700

    Menambahkan logika circular queue pada REAR dan menyimpan data ke dalam queue

--- Queue.cpp
+++ Queue.cpp
@@ -37,3 +37,13 @@ public:
         }
         else
         {
+            // jika rear berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
+    }
+
+    
